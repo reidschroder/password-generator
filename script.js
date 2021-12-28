59fa5a5011881981db715e837d4d4a659f91c490
Reid Schroder <reidschroder2@gmail.com>
2021-12-28 02:42:21 -0500

    added constants, prompts, confirms, and empty if statements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Constants
+const numbers = "0123456789";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialCharacters = "!@#$%^&*()";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,6 +13,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+// Prompt Section
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for your random password. Password must be at least 8, but no more than 128 characters.");
+
+  var numbers = confirm("Would you like numbers in your password?");
+
+  var lowerCase = confirm("Would you like lowercase letters in your password?");
+
+  var upperCase = confirm("Would you like uppercase letters in your password?");
+
+  var specialCharacters = confirm("Would you like special characters in your password?");
+}
+
+// If statements for prompt answers
+
+if (numbers === true) {
+
+}
+
+if (lowerCase === true) {
+
+}
+
+if (upperCase === true) {
+
+}
+
+if (specialCharacters === true) {
 
 }
 
