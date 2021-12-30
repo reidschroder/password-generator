57e763a0acf1bdfa1f7708e061356d74d57d0a5a
Reid Schroder <reidschroder2@gmail.com>
2021-12-30 12:05:11 -0500

    cleaned if statements, added for loop, added if statement to passwordLength

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Constants
-const numbers = "0123456789";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const specialCharacters = "!@#$%^&*()";
+const numbersStr = "0123456789";
+const lowerCaseStr = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const specialCharactersStr = "!@#$%^&*()";
+var chosenCharactersStr = "";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -19,7 +20,11 @@ function writePassword() {
 // Prompt Section
 function generatePassword() {
   var passwordLength = prompt("Please enter the number of characters you want for your random password. Password must be at least 8, but no more than 128 characters.");
-
+    
+  if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please enter a valid number");
+      passwordLength();
+  }
   var numbers = confirm("Would you like numbers in your password?");
 
   var lowerCase = confirm("Would you like lowercase letters in your password?");
@@ -27,26 +32,29 @@ function generatePassword() {
   var upperCase = confirm("Would you like uppercase letters in your password?");
 
   var specialCharacters = confirm("Would you like special characters in your password?");
-}
-
-
 // If statements for prompt answers
 
-if (numbers === true) {
+if (numbers) chosenCharactersStr += numbersStr;
+    
+if (lowerCase) chosenCharactersStr += lowerCaseStr;
+  
+if (upperCase) chosenCharactersStr += upperCaseStr;
+ 
+if (specialCharacters) chosenCharactersStr += specialCharactersStr;
 
+var finalPassword = "";
+for (var i = 0; i < parseInt(passwordLength); i++) {
+    var randomIndex = Math.floor(Math.random() * chosenCharactersStr.length);
+    finalPassword += chosenCharactersStr[randomIndex];
 }
 
-if (lowerCase === true) {
 
+  
+  return finalPassword;
 }
 
-if (upperCase === true) {
 
-}
 
-if (specialCharacters === true) {
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
